8b3f45fd2ee7b1dc519f9328fa2afdb0e9860bfe
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2024-08-14 18:20:25 +0200

    files terug gezet hoe ze oorspronkelijk stonden

--- config/production.js
+++ config/production.js
@@ -1,5 +1,8 @@
 module.exports = {
-    port: process.env.PORT || 9000,
+    database: {
+        client: "mysql2",
+    },
+    port: 9000,
     auth: {
         argon: {
             saltLength: 16,
@@ -8,12 +11,13 @@ module.exports = {
             memoryCost: 2 ** 17,
         },
         jwt: {
-            secret: process.env.AUTH_JWT_SECRET,
+            secret: "eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked",
             expirationInterval: 60 * 60 * 1000,
             issuer: "2324-WEBSERVICES-JASPERHMN",
             audience: "2324-WEBSERVICES-JASPERHMN",
         },
     },
+
     log: {
         level: "silly",
         disabled: false,
@@ -21,13 +25,5 @@ module.exports = {
     cors: {
         origins: ["https://frontendweb-motor.onrender.com"],
         maxAge: 3 * 60 * 60,
-    },
-    database: {
-        client: "mysql2",
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        name: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
     },
 };
