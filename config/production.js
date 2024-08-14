b5676e6343aae72dfc63b7e5b39c2e22911a3b28
JASPER HAEGEMAN <JASPER.HAEGEMAN@student.hogent.be>
2024-08-14 17:57:40 +0200

    production.js aangepast

--- config/production.js
+++ config/production.js
@@ -1,8 +1,5 @@
 module.exports = {
-    database: {
-        client: "mysql2",
-    },
-    port: 9000,
+    port: process.env.PORT || 9000,
     auth: {
         argon: {
             saltLength: 16,
@@ -11,13 +8,12 @@ module.exports = {
             memoryCost: 2 ** 17,
         },
         jwt: {
-            secret: "eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked",
+            secret: process.env.AUTH_JWT_SECRET,
             expirationInterval: 60 * 60 * 1000,
             issuer: "2324-WEBSERVICES-JASPERHMN",
             audience: "2324-WEBSERVICES-JASPERHMN",
         },
     },
-
     log: {
         level: "silly",
         disabled: false,
@@ -25,5 +21,13 @@ module.exports = {
     cors: {
         origins: ["https://frontendweb-motor.onrender.com"],
         maxAge: 3 * 60 * 60,
+    },
+    database: {
+        client: "mysql2",
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        name: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
     },
 };
